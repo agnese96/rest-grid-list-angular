c3c1530b4e18ce3bca5c2789f1cfba1428563fc7
Agnese96 <agnesefolli@gmail.com>
2016-09-14 15:14:58 +0100

    [testing] fixing deployment issues

--- src/main/java/hello/ControllerGetData.java
+++ src/main/java/hello/ControllerGetData.java
@@ -12,6 +12,11 @@ public class ControllerGetData {
 	@GetMapping("/get-data")
 	public List<GridItem> getGridList(){
 		GridListDAO dao= new GridListDAO();
+		GridItem test= new GridItem();
+		test.title="prova";
+		test.background="http://lh3.googleusercontent.com/-kqYyvKLHkcY/Vb-b6o560hI/AAAAAAAAQdU/ULOSJRDPg-E/clip_image001_thumb.png?imgmax=800";
+		test.description="This is merely a test!";
+		dao.insertListItem(test);
 		List<GridItem> items=dao.getList();
 		return items;
 	}
